8e4ed0c99e71acc918020fd41e4c19b206c996c6
Ravano Akbar Widodo <118270814+ravano-2464@users.noreply.github.com>
2024-02-23 06:29:10 +0700

    app terkait informasi data pemilu

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <div className="px-36 py-10">
       <div className="text-white">
         <h5 className="text-[20px] text-center">
-          Komisi Pemilihan Umum DumbWays.ID | Malik Mahdi 2023
+          Komisi Pemilihan Umum DumbWays.ID | Ravano Akbar Widodo 2023
         </h5>
       </div>
     </div>
