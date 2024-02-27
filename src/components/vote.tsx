4fc1c53f4c6bac4ed9d931f04f00a4f5fc5f65dd
Ravano Akbar Widodo <118270814+ravano-2464@users.noreply.github.com>
2024-02-27 13:59:19 +0700

    app terkait informasi data pemilu

--- src/components/vote.tsx
+++ src/components/vote.tsx
@@ -12,7 +12,6 @@ const Vote: React.FC = () => {
     };
 
     return (
-
         <>
             <Navbar />
             <div className="relative body">
@@ -28,7 +27,6 @@ const Vote: React.FC = () => {
                             <div className="ml-[30px] text-4xl font-black text-left text-red-800 ">
                                 <h1>HIROHITO</h1>
                                 <h1>78%</h1>
-
                             </div>
                         </div>
                         <div className="flex flex-row w-[657px] p-4 bg-amber-300 shadow-amber-300 shadow-lg mt-7 rounded-2xl h-[]146px">
@@ -38,7 +36,6 @@ const Vote: React.FC = () => {
                             <div className="ml-[30px] text-4xl font-black text-left text-red-800 ">
                                 <h1>ADOLF HITLER</h1>
                                 <h1>45%</h1>
-
                             </div>
                         </div>
                         <div className="flex flex-row w-[657px] p-4  bg-sky-400 shadow-sky-400  shadow-lg mt-7 rounded-2xl h-[]146px">
@@ -51,48 +48,47 @@ const Vote: React.FC = () => {
                             </div>
                         </div>
                         <div className="bgc2 flex justify-center w-[466px] p-4 rounded-lg mt-10 font-bold hover:bg-lime-700">
-                           <button onClick={goToSModal} className="text-[32px] text-center">MASUKAN SUARAMU</button>
+                            <button onClick={goToSModal} className="text-[32px] text-center">MASUKAN SUARAMU</button>
                         </div>
-                        </div>
-                        <div className=" h-[500px] bg-slate-300 ps-[100px]">
-                        <h2 className="mt-10 mb-2 text-5xl font-black text-center text ">INFO PASLON</h2>
-                            <div className="flex flex-row flex justify-center  mx-auto mt-10 p-4 bg-white rounded-lg shadow-md w-[650px] h-[494]">
-                             <div className="mt-6 img">
-                                <img src={hirohito} alt="hirohito"></img>
+                        <br />
+                    </div>
+                    <div className="flex justify-center items-center h-[650px] bg-slate-300">
+                        <div>
+                            <h2 className="mt-10 mb-2 text-5xl font-black text-center text">INFO PASLON</h2>
+                            <div className="flex flex-row justify-center mx-auto mt-10 p-4 bg-white rounded-lg shadow-md w-[650px] h-[494]">
+                                <div className="mt-6 img">
+                                    <img src={hirohito} alt="hirohito" />
                                 </div>
                                 <div className="flex flex-col ml-10">
-                                <p className="mb-2 font-bold -lg">Nomor Urut : 1</p>
-                                <p className="mb-4 text-3xl font-bold ">HIROHITO</p>
-                                <h3 className="mb-2 font-bold text-">Visi & Misi :</h3>
-                                <ul className="mb-4 text-lg list-disc list-inside">
-                                    <li>Memindahkan Indonesia ke Isekai.</li>
-                                    <li>Nonton anime 3x sehari.</li>
-                                    <li>Melakukan peresapan pada budaya jepang.</li>
-                                </ul>
-                                <h3 className="mb-2 text-lg font-bold">Koalisi :</h3>
-                                <ul className="text-lg list-disc list-inside">
-                                    <li>Partai Persatuan Wiboo.</li>
-                                    <li>Partai Redbull.</li>
-                                    <li>Partai Black Magic.</li>
-                                </ul>
+                                    <p className="mb-2 font-bold -lg">Nomor Urut : 1</p>
+                                    <p className="mb-4 text-3xl font-bold">HIROHITO</p>
+                                    <h3 className="mb-2 font-bold">Visi & Misi :</h3>
+                                    <ul className="mb-4 text-lg list-disc list-inside">
+                                        <li>Memindahkan Indonesia ke Isekai.</li>
+                                        <li>Nonton anime 3x sehari.</li>
+                                        <li>Melakukan peresapan pada budaya jepang.</li>
+                                    </ul>
+                                    <h3 className="mb-2 text-lg font-bold">Koalisi :</h3>
+                                    <ul className="text-lg list-disc list-inside">
+                                        <li>Partai Persatuan Wiboo.</li>
+                                        <li>Partai Redbull.</li>
+                                        <li>Partai Black Magic.</li>
+                                    </ul>
                                 </div>
                             </div>
                         </div>
-                        <div className="flex justify-center text-4xl font-bold text-center text-red-600 py-28">
-                            
-                                <h1>
-                                    PILIH BERDASARKAN GACHA TIDAK USAH SERIUS
-                                    <br/>YANG PENTING TIDAK MELEGALKAN SLOT
-                                </h1>
-                           
-
-                        </div>
                     </div>
-                
+                    <div className="flex justify-center text-4xl font-bold text-center text-red-600 py-28">
+                        <h1>
+                            PILIH BERDASARKAN GACHA TIDAK USAH SERIUS
+                            <br />YANG PENTING TIDAK MELEGALKAN SLOT
+                        </h1>
+                    </div>
+                </div>
             </div>
             <Footer />
         </>
-    )
-
+    );
 }
+
 export default Vote;
